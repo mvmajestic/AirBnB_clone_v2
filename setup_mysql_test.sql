79e5a1e993a7072e7a3af7ea47008c1c6c093f1e
Vuyisanani <vuyisanani@ymail.com>
2024-02-20 06:35:08 -0800

    Fixed Errors on MySQL Setup Test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,9 @@
---creates a database a new user with specified permissions
+-- Set ALL privileges on the database `hbnb_dev_db` to the `hbnb_dev` user.
+-- Set SELECT privileges on the database `performance_schema` to the `hbnb_dev` user.
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+DROP USER IF EXISTS 'hbnb_test'@'localhost';
+CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
