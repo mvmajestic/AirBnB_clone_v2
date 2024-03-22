d7fc534f5481befbc917aa1ae99d7d372a8b5e8c
Vuyisanani Mkutyukelwa <vuyisanani@ymail.com>
2024-03-22 11:38:13 -0700

    Updated Task 0: Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,10 +9,11 @@ Routes:
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 if __name__ == "__main__":
+    app.url_map.strict_slashes = False
     app.run(host="0.0.0.0")
