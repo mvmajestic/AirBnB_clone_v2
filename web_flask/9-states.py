dcb4761628e1becfa6849232476a382e72b2b71e
Vuyisanani Mkutyukelwa <vuyisanani@ymail.com>
2024-03-24 12:48:03 -0700

    Updated Errors

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -13,19 +13,16 @@ from models.state import State
 
 app = Flask(__name__)
 
-
 @app.teardown_appcontext
 def close(self):
     """ Method to close the session """
     storage.close()
-
 
 @app.route('/states', strict_slashes=False)
 def state():
     """Displays a html page with states"""
     states = storage.all(State)
     return render_template('9-states.html', states=states, mode='all')
-
 
 @app.route('/states/<id>', strict_slashes=False)
 def state_by_id(id):
@@ -34,7 +31,6 @@ def state_by_id(id):
         if state.id == id:
             return render_template('9-states.html', states=state, mode='id')
     return render_template('9-states.html', states=state, mode='none')
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
