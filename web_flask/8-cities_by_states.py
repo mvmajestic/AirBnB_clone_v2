aa577396a2de719884ae966ed1e9c177b51f0703
Vuyisanani Mkutyukelwa <vuyisanani@ymail.com>
2024-03-24 12:26:59 -0700

    Task 9:

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Script will start a Flask Web application.
+
+Application listens on 0.0.0.0, port 5000.
+Routes:
+    /cities_by_states: HTML page with a list of all states and related cities.
+"""
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states')
+def states_list():
+    """Render template with states
+    """
+    path = '8-cities_by_states.html'
+    states = storage.all(State)
+
+    # sort State object alphabetically by name
+    # sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, states=states)
+
+
+@app.teardown_appcontext
+def app_teardown(arg=None):
+    """Clean-up session
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
